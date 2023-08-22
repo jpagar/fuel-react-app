d33a104d36582a24824c7dc6fdfb00f2dd1dbef7
jonathanpagar <jcigar03@gmail.com>
2023-08-21 21:20:53 -0400

    This is a playground branch for trying out new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import SidebarContext from "./context/SideBarContext.tsx";
+import { Menu } from "lucide-react";
+
 import { useState } from "react";
-import { SidebarNavigation } from "./pages/SidebarNavigation.tsx";
-import { Outlet } from "react-router-dom";
 
 function App() {
   const [expand, setExpand] = useState(false);
+  const handleClick = () => {
+    setExpand((prevState) => !prevState);
+  };
+  const handleBackDropClick = () => {
+    setExpand(false);
+  };
+
   return (
-    <>
-      <div className="flex">
-        <SidebarContext.Provider
-          value={{ expand: expand, setExpand: setExpand }}
-        >
-          <SidebarNavigation />
-          {/* TODO: Find a better solution to expand nav bar over content */}
-          <Outlet />
-        </SidebarContext.Provider>
-      </div>
-    </>
+      <>
+        <Menu className="m-3 cursor-pointer" onClick={handleClick} />
+        <div
+            className={`absolute top-0 z-10 h-screen w-40 rounded-r bg-zinc-800 shadow translate-x-[${
+                expand ? "0%" : "-100%"
+            }] border border-solid border-black transition-all`}
+        ></div>
+        <div
+            onClick={handleBackDropClick}
+            className={`z-[0] h-screen w-screen bg-gray-200 backdrop-grayscale-0`}
+        />
+
+      </>
   );
 }
 
