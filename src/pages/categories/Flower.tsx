370d67f3bbeb2ae47e7fe332c5e3032b18e6c5c8
jonathanpagar <jcigar03@gmail.com>
2023-08-24 22:08:26 -0400

    Modified FlowerComponent to dynamically pull products from data source. Also added zustand store for global states

--- src/pages/categories/Flower.tsx
+++ src/pages/categories/Flower.tsx
@@ -1,83 +1,31 @@
-import {ScrollableContent} from "../../components/ScrollableContent.tsx";
-import {Card} from "../../components/Card.tsx";
+import { ScrollableContent } from "../../components/ScrollableContent.tsx";
+import { Card } from "../../components/Card.tsx";
+import { products, useStore } from "../../store.ts";
+import { useEffect } from "react";
 
 export const Flower = () => {
-    return (
-        <>
-            <ScrollableContent className="">
-                <Card
-                    title="Designer Runtz"
-                    img="https://weedmaps.com/images/photos/products/000/239/061/710648_TerryT-Gelato-Square2.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Cookie Platter - Top Shelf"
-                    img="https://weedmaps.com/images/products/000/095/327/avatar/1671844083-curejoint_3_pack_cookie_platter.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Marathon OG"
-                    img="https://weedmaps.com/images/photos/products/000/070/686/344188_MarathonOG-thecurecompany-weedmaps-box-nug.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Ghost Face Mintz"
-                    img="https://weedmaps.com/images/photos/products/000/335/015/343492_WM_3000X3000_GHOST_FACE_MINTZ__H_.png?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="3.5g Hybrid Flower"
-                />
-                <Card
-                    title="Designer Runtz"
-                    img="https://weedmaps.com/images/photos/products/000/239/061/710648_TerryT-Gelato-Square2.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Cookie Platter - Top Shelf"
-                    img="https://weedmaps.com/images/products/000/095/327/avatar/1671844083-curejoint_3_pack_cookie_platter.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Marathon OG"
-                    img="https://weedmaps.com/images/photos/products/000/070/686/344188_MarathonOG-thecurecompany-weedmaps-box-nug.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Ghost Face Mintz"
-                    img="https://weedmaps.com/images/photos/products/000/335/015/343492_WM_3000X3000_GHOST_FACE_MINTZ__H_.png?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="3.5g Hybrid Flower"
-                />
-                <Card
-                    title="Designer Runtz"
-                    img="https://weedmaps.com/images/photos/products/000/239/061/710648_TerryT-Gelato-Square2.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Cookie Platter - Top Shelf"
-                    img="https://weedmaps.com/images/products/000/095/327/avatar/1671844083-curejoint_3_pack_cookie_platter.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Marathon OG"
-                    img="https://weedmaps.com/images/photos/products/000/070/686/344188_MarathonOG-thecurecompany-weedmaps-box-nug.jpg?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="Super duper fuego. It will knock your socks off!"
-                />
-                <Card
-                    title="Ghost Face Mintz"
-                    img="https://weedmaps.com/images/photos/products/000/335/015/343492_WM_3000X3000_GHOST_FACE_MINTZ__H_.png?auto=format&fit=fill&fill=solid&fill-color=fff&w=540&h=540"
-                    alt="Designer Runtz"
-                    description="3.5g Hybrid Flower"
-                />
-            </ScrollableContent>
-        </>
-    );
+  const { addToCart, cart } = useStore();
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  return (
+    <>
+      <ScrollableContent className="">
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            title={product.name}
+            img={product.image}
+            alt={product.name}
+            description={
+              "Some description would be added here. Maybe a short description of the product."
+            }
+            onClick={() => addToCart(product)}
+          />
+        ))}
+      </ScrollableContent>
+    </>
+  );
 };
