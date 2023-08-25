42db17a9d228cfa405e9906715cf2723c90ef224
jonathanpagar <jcigar03@gmail.com>
2023-08-24 22:10:40 -0400

    Added a badge count next to Cart icon. Indicates count of items in cart.

--- src/pages/SidebarNavigation.tsx
+++ src/pages/SidebarNavigation.tsx
@@ -1,13 +1,15 @@
 import {Sidebar} from "../components/Sidebar.tsx";
 import {Boxes, LifeBuoy, Package, Settings, ShoppingCart} from "lucide-react";
 import {SidebarItem} from "../components/SidebarItem.tsx";
+import {useStore} from "../store.ts";
 
 
 export const SidebarNavigation = () => {
+    const {getShoppingCartQuantity} = useStore()
     return (
         <Sidebar>
             <SidebarItem icon={<Boxes size={20}/>} text="Shop" to="shop"/>
-            <SidebarItem icon={<ShoppingCart size={20}/>} text="Cart" to="cart"/>
+            <SidebarItem badgeText={String(getShoppingCartQuantity())} hasBadge={true} icon={<ShoppingCart size={20}/>} text="Cart" to="cart"/>
             <SidebarItem icon={<Package size={20}/>} text="Orders" to="orders"/>
             <SidebarItem icon={<Settings size={20}/>} text="Settings" to="settings"/>
             <SidebarItem icon={<LifeBuoy size={20}/>} text="Help" to="help"/>
