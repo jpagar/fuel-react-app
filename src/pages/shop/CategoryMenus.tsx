791b5da4d3883c13f6e1f990f1db5c849cb7b1cd
jonathanpagar <jcigar03@gmail.com>
2023-08-26 17:43:57 -0400

    added set category on tiles to help filter product listing

--- src/pages/shop/CategoryMenus.tsx
+++ src/pages/shop/CategoryMenus.tsx
@@ -1,13 +1,32 @@
 import { Categories } from "../../components/Categories.tsx";
 import { MenuTile } from "../../components/MenuTile.tsx";
+import { useCategoryStore } from "../../stores/useCategoryStore.ts";
 
 export const CategoryMenus = () => {
+  const {setCategory } = useCategoryStore();
+
   return (
     <Categories pageTitle="Shop by Category">
-      <MenuTile text="Flower" to="flower" />
-      <MenuTile text="Edibles" to="edibles" />
-      <MenuTile text="Vape Pens" to="vape-pens" />
-      <MenuTile text="Concentrates" to="concentrates" />
+      <MenuTile
+        text="Flower"
+        to="flower"
+        onClick={() => setCategory("flower")}
+      />
+      <MenuTile
+        text="Edibles"
+        to="edibles"
+        onClick={() => setCategory("edible")}
+      />
+      <MenuTile
+        text="Vape Pens"
+        to="vape-pens"
+        onClick={() => setCategory("vape")}
+      />
+      <MenuTile
+        text="Concentrates"
+        to="concentrates"
+        onClick={() => setCategory("concentrate")}
+      />
     </Categories>
   );
 };
