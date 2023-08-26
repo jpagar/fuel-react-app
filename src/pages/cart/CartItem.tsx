f4cbf22cdb8c9d9d73d120ceae38a34bf9f71e89
jonathanpagar <jcigar03@gmail.com>
2023-08-26 17:42:57 -0400

    Added individual total price

--- src/pages/cart/CartItem.tsx
+++ src/pages/cart/CartItem.tsx
@@ -1,3 +1,5 @@
+import {FC} from "react";
+
 type CartItemProps = {
   name: string;
   price: string;
@@ -7,14 +9,14 @@ type CartItemProps = {
   onClick?: () => void;
 };
 
-export const CartItem = ({
+export const CartItem: FC<CartItemProps> = ({
   name,
   image,
   price,
   description,
   quantity,
   onClick,
-}: CartItemProps) => {
+}) => {
   return (
     <li className="h-1/12 mx-2 mt-3 flex border-t border-gray-300 py-2">
       <div className="w-4/12">
@@ -27,26 +29,31 @@ export const CartItem = ({
             <p className="text-sm">{description}</p>
           </div>
           <p className="flex gap-1">
-            <span>${price}</span><span>{quantity ? `- ${quantity}x` : null}</span>
+            <div>
+              <span>${price}</span><span>{quantity ? `- ${quantity}x` : null}</span>
+              <p className="flex justify-end text-gray-500">${quantity && Number(price) * quantity}</p>
+            </div>
           </p>
         </div>
         <div className="mt-4 flex gap-6">
-          <select className="rounded border border-solid border-gray-300 px-4 py-1 text-sm md:text-base">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-          </select>
+          {/*<select className="rounded border border-solid border-gray-300 px-4 py-1 text-sm md:text-base">*/}
+          {/*  <option value="1">1</option>*/}
+          {/*  <option value="2">2</option>*/}
+          {/*  <option value="3">3</option>*/}
+          {/*  <option value="4">4</option>*/}
+          {/*  <option value="5">5</option>*/}
+          {/*  <option value="6">6</option>*/}
+          {/*  <option value="7">7</option>*/}
+          {/*  <option value="8">8</option>*/}
+          {/*  <option value="9">9</option>*/}
+          {/*</select>*/}
 
-          {/*TODO: Add a modal to ask user to confirm if they want to remove items from cart*/}
           <button onClick={onClick} className="bg-red-300 px-2 py-0.5 rounded cursor-pointer">
             Remove
           </button>
+
+          {/*TODO: Need to make dialog element's id dynamic so that modal overlay works*/}
+          {/*<Modal name="Remove" onClick={onClick}/>*/}
         </div>
       </div>
     </li>
