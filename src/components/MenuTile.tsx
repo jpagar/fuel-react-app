a7960a9a21d013dd49d9c66989e48b1fa4070d1c
jonathanpagar <jcigar03@gmail.com>
2023-08-26 17:45:05 -0400

    added prop extension of HTML Attributes

--- src/components/MenuTile.tsx
+++ src/components/MenuTile.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import React, {FC} from "react";
 
-type MenuTileProps = {
+interface MenuTileProps extends React.HTMLAttributes<HTMLAnchorElement> {
   text: string;
   to: string;
-};
+}
 
-export const MenuTile = ({ text, to }: MenuTileProps) => {
+export const MenuTile: FC<MenuTileProps> = ({ text, to, ...props}) => {
   return (
-    <Link to={to}>
+    <Link to={to} {...props}>
       {
         <div className="d-btn d-btn-outline w-full cursor-pointer rounded px-6 py-3 shadow-lg">
           {text}
